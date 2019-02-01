f52a352b5d8f5a69a6b3523c8104b6b2c039fc92
Jonathan Ohayon <me@jonathano.com>
2019-02-01 18:45:24 +0200

    Added center, right, left distances

--- src/main/java/com/team3316/kit/path/Segment.java
+++ src/main/java/com/team3316/kit/path/Segment.java
@@ -3,12 +3,14 @@ package com.team3316.kit.path;
 import de.siegmar.fastcsv.reader.CsvRow;
 
 public class Segment {
-  private double _time, _heading, _centerDist, _velocity, _acceleration;
+  private double _time, _heading, _centerDist, _leftDist, _rightDist, _velocity, _acceleration;
 
   public Segment (CsvRow csv) {
     this._time = Double.parseDouble(csv.getField("t"));
     this._heading = Double.parseDouble(csv.getField("theta"));
     this._centerDist = Double.parseDouble(csv.getField("s"));
+    this._leftDist = Double.parseDouble(csv.getField("sl"));
+    this._rightDist = Double.parseDouble(csv.getField("sr"));
     this._velocity = Double.parseDouble(csv.getField("v"));
     this._acceleration = Double.parseDouble(csv.getField("a"));
   }
@@ -17,12 +19,16 @@ public class Segment {
     return this._heading;
   }
 
+  public double getCenterDist () {
+    return this._centerDist;
+  }
+
   public double getLeftDist () {
-    return this._centerDist; // TODO - Implement s-theta*r
+    return this._leftDist;
   }
 
   public double getRightDist () {
-    return this._centerDist; // TODO - Implement s+theta*r
+    return this._rightDist;
   }
 
   public double getVelocity () {
