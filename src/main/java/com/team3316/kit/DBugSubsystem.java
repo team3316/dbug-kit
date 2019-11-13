f15ad54e91b6d5181babf5a6c654f175f040cba7
Ido Bleiberg <idoblibarge@gmail.com>
2019-11-13 12:51:12 +0200

    Deleted ussles logger from subsystem.

--- src/main/java/com/team3316/kit/DBugSubsystem.java
+++ src/main/java/com/team3316/kit/DBugSubsystem.java
@@ -1,10 +1,8 @@
 package com.team3316.kit;
 
-import com.team3316.kit.config.ConfigException;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public abstract class DBugSubsystem extends Subsystem {
-  DBugLogger logger = DBugLogger.getInstance();
 
   public abstract void initDefaultCommand();
 
