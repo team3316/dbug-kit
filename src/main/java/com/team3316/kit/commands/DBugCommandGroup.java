c0939d3f46f4e4b95b1e19750b247a8652753f89
tommykark <tommyka03@gmail.com>
2020-01-25 18:07:56 +0200

    command group updates & fixes

--- src/main/java/com/team3316/kit/commands/DBugCommandGroup.java
+++ src/main/java/com/team3316/kit/commands/DBugCommandGroup.java
@@ -1,7 +1,9 @@
 package com.team3316.kit.commands;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -10,20 +12,26 @@ import com.team3316.kit.DBugLogger;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
- * DBugCommandBaseGroup
+ * DBugCommandGroup
  */
 public abstract class DBugCommandGroup extends DBugCommand {
     private List<List<Supplier<CommandBase>>> queue = new LinkedList<>();
     private List<List<Supplier<CommandBase>>> queueStorage = new LinkedList<>();
-    private List<CommandBase> parallelsList;
+    private Map<CommandBase, Boolean> parallelsDict;
 
     boolean canAdd = true;
     boolean isFinished;
 
-    public DBugCommandGroup() {};
+    /**
+     * Constructor - shall be overridden when writing command groups
+     */
+    public DBugCommandGroup() {
+    };
 
-
-
+    /**
+     * @return The command group's representation of the commands that shall run -
+     *         in the hierarchy
+     */
     public List<List<Supplier<CommandBase>>> getStorage() {
         return this.queueStorage;
     }
@@ -38,62 +46,94 @@ public abstract class DBugCommandGroup extends DBugCommand {
         queue = new LinkedList<>(queueStorage);
     }
 
-
     @Override
     public void execute() {
-        if (parallelsList == null) {
+
+        if (parallelsDict == null) {
             this._runNextSequential();
-        } else if (parallelsList.stream().allMatch(cmd -> !cmd.isScheduled())) {
-            DBugLogger.getInstance().info("SEQUENTIAL ENDED");
-            if (!queue.isEmpty()) {
-                this._runNextSequential();
-            } else {
-                isFinished = true;
+        } else {
+            for (CommandBase cmd : parallelsDict.keySet()) {
+                if (cmd.isFinished()) {
+                    parallelsDict.put(cmd, true);
+                }
+            }
+
+            if (parallelsDict.entrySet().stream()
+                    .anyMatch((entry) -> !entry.getKey().isScheduled() && !entry.getValue())) {
+                this.cancel();
+            } else if (parallelsDict.entrySet().stream().allMatch(entry -> !entry.getKey().isScheduled())) {
+                DBugLogger.getInstance().info(this.getClass().getName() + " SEQUENTIAL ENDED");
+                if (!queue.isEmpty()) {
+                    this._runNextSequential();
+                } else {
+                    isFinished = true;
+                }
             }
         }
+
     }
 
+    /**
+     * runs the next parallel sequence of command that should run 
+     */
     private void _runNextSequential() {
-        DBugLogger.getInstance().info("SEQUENTIAL STARTED");
+        DBugLogger.getInstance().info(this.getClass().getName() + " SEQUENTIAL STARTED");
         List<Supplier<CommandBase>> list = queue.remove(0);
-        parallelsList = new LinkedList<>();
+        parallelsDict = new HashMap<>();
 
         for (Supplier<CommandBase> sup : list) {
             CommandBase cmd = sup.get();
             cmd.schedule();
-            parallelsList.add(cmd);
+            parallelsDict.put(cmd, false);
         }
     }
 
-
     /**
-     * @param args - appends a sequence of CommandBases to run. Given as suppliers, each returning a new CommandBase instance. These will run simultaneously.
+     * @param args - appends a sequence of CommandBases to run. Given as suppliers,
+     *             each returning a new CommandBase instance. These will run
+     *             simultaneously.
      *
      */
     @SafeVarargs
-    public final synchronized void add(Supplier<CommandBase>... args){
-        List<Supplier<CommandBase>> l = new LinkedList<>();
+    protected final synchronized void add(Supplier<CommandBase>... args) {
 
-        for (int i = 0; i < args.length; i++) {
-            l.add(args[i]);
-        }
+        List<Supplier<CommandBase>> l = List.of(args);
 
         if (canAdd) {
             queueStorage.add(l);
         } else {
             StringBuilder sb = new StringBuilder();
-            sb.append("Tried to add sequential after initalizing command group ");
+            sb.append("Tried to add sequential after initializing command group ");
             sb.append(this.getName());
             sb.append(" [");
             sb.append(l.stream().map(sup -> ((Supplier<CommandBase>) sup).get().getClass().getName())
-                .collect(Collectors.joining(", ")));
+                    .collect(Collectors.joining(", ")));
             sb.append("]");
             DBugLogger.getInstance().severe(sb.toString());
         }
     }
 
+    /**
+     * runs before the <code>end(boolean interrupted)</code> function.
+     * @param interrupted - whether the command was interrupted or ended naturally.
+     */
     @Override
-    public boolean isFinished() {
+    public final void fin(boolean interrupted) {
+        if (interrupted && !queue.isEmpty()) {
+            DBugLogger.getInstance().info(this.getClass().getName() + " INTERRUPTED");
+            parallelsDict.keySet().stream().forEach(cmd -> cmd.cancel());
+        } else {
+            DBugLogger.getInstance().info(this.getClass().getName() + " ENDED");
+        }
+        queue = new LinkedList<>();
+        parallelsDict = new HashMap<>();
+    }
+
+    /**
+     * @return whether the command's end condition has been reached.
+     */
+    @Override
+    public final boolean isFinished() {
         return isFinished;
     }
 
